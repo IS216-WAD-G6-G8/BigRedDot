78b1e3b728d5dd4fd05cfa4cd840a484ef9a0609
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2022-10-20 03:52:33 +0800

    Rebuilt category landing page

--- src/components/Pages/LandingPage.vue
+++ src/components/Pages/LandingPage.vue
@@ -1,3 +1,62 @@
-<script>
-
+<script lang="ts">
+export default {
+    name: 'LandingPage',
+    data() {
+        return {
+            categories: [
+            {name:"services", url:"/assets/services.jpg"},
+            {name:"f&b", url:"/assets/f&b.jpg"},
+            {name:"fashion", url:"/assets/fashion.jpg"},
+            {name:"crafts", url:"/assets/crafts.jpg"},
+            {name:"experiences", url:"/assets/experiences.jpeg"},
+          ]
+        }
+    },
+    methods: {},
+}
 </script>
+
+<template>
+    <div class="carousel_container">
+        <div v-for="category of categories" class="carousel_box">
+            <p class="carousel_text">{{category.name}}</p>
+            <img
+                class="carousel_img"
+                :src="category.url"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.carousel_container {
+   display: flex;
+   height: 100vh;
+ }
+ .carousel_text {
+   font-weight: bold;
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+   visibility: hidden;
+ }
+ .carousel_img {
+   object-fit: cover;
+   height: 100%;
+   width: 100%;
+ }
+ .carousel_box {
+   flex: 1;
+   transition: 1s;
+   position: relative;
+ }
+ .carousel_box:hover .carousel_text {
+   visibility: visible;
+ }
+ .carousel_box:hover .carousel_img {
+   opacity: 20%;
+ }
+ .carousel_box:hover {
+   flex: 4;
+ }
+</style>
