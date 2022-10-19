a85603d588b5d077eee38e2b4c3fe7c57d641fb9
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2022-10-20 02:55:45 +0800

    Fixed nav bar + Building category filter component

--- src/components/Pages/Home.vue
+++ src/components/Pages/Home.vue
@@ -1,17 +1,37 @@
 <script lang="ts">
 import NavBar from "../Reusables/NavBar.vue"
+
 export default {
     name: "Home",
     data() {
-        return {};
+        return {
+          categories: [
+            {name:"services", url:"/assets/services.svg"},
+            {name:"f&b", url:"/assets/f&b.svg"},
+            {name:"fashion", url:"/assets/fashion.svg"},
+            {name:"crafts", url:"/assets/crafts.svg"},
+            {name:"experiences", url:"/assets/experiences.svg"},
+          ]
+        };
     },
-    methods: {},
+    methods: {
+    },
     components: { NavBar }
 }
 </script>
 
 <template>
   <NavBar></NavBar>
+  <!-- Build filter section -->
+  <div class="bg-[#A4CAFE2E] px-5 w-full flex justify-between">
+    <div class="container flex flex-wrap justify-between mx-auto">
+      <div v-for="category in categories" class="px-3 py-3 flex flex-col items-center">
+        <img :src="category.url"  class="w-6 h-6 mb-2">
+        <span class="text-xs md:text-sm">{{category.name}}</span>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped></style>
