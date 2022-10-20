8bae67d429687c84f1dee441f73e49b57d868fc2
nicamanas <xyukimiku@gmail.com>
2022-10-20 15:35:57 +0800

    firebase hosting integration (wip)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router' 
+import router from './router'
+
+import firebase from 'firebase/compat/app'
+import * as firebaseui from 'firebaseui'
+import 'fireebaseui/dist/firebaseui.css'
 
 createApp(App).use(router).mount('#app')
