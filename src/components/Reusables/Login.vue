f297f80bb4c0e1018711caf74f27d4f4b29b4276
nicamanas <xyukimiku@gmail.com>
2022-10-22 00:46:13 +0800

    default login test

--- src/components/Reusables/Login.vue
+++ src/components/Reusables/Login.vue
@@ -1,22 +1,54 @@
 <script lang="ts">
+import firebase from 'firebase/compat/app'
+import * as  firebaseui from 'firebaseui'
+import "firebaseui/dist/firebaseui.css"
+
+
+
 export default {
     name: 'Login',
     el: "#app",
     data() {
         return {
-            
+            user: null
         }
     },
+    mounted() {
+        let ui = firebaseui.auth.AuthUI.getInstance()
+        if (!ui) {
+            ui = new firebaseui.auth.AuthUI(firebase.auth())
+        }
+        var uiConfig = {
+            signInSuccessUrl: "/Landing",
+            signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID, firebase.auth.EmailAuthProvider.PROVIDER_ID]
+        }
+        ui.start("#firebaseui-auth-container", uiConfig)
+    },
+    created() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.user = user
+                console.log(user)
+            }
+        })
+    }
     
 }
 
 </script>
 
 <template>
-    <li class="w-full md:w-auto mb-1 mt-3 md:mt-0 md:mb-0">
+    <section id="firebaseui-auth-container">
+    <li v-show="!user" class="w-full md:w-auto mb-1 mt-3 md:mt-0 md:mb-0">
         <button
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded w-full md:w-auto">
             Log In / Sign Up
+        </button>
+    </li>
+    <li v-show="user" class="w-full md:w-auto mb-1 mt-3 md:mt-0 md:mb-0">
+        <button
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-5 rounded w-full md:w-auto">
+            Log Out
         </button>
     </li>
     <!-- <li class="w-full md:w-auto mb-1 mt-3 md:mt-0 md:mb-0">
@@ -25,4 +57,5 @@ export default {
             Sign Up
         </button>
     </li> -->
+    </section>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
+import firebase from 'firebase/compat/app';
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './router';
 import Login from './components/Reusables/Login.vue';
-// import 'fireebaseui/dist/firebaseui.css'
+const firebaseConfig = {
+    apiKey: 'AIzaSyAipvvlbfDtRMXnN_UDeHl67l-XbgWc_CU',
+    authDomain: 'is216-bigreddot.firebaseapp.com',
+    databaseURL: 'https://is216-bigreddot-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'is216-bigreddot',
+    storageBucket: 'is216-bigreddot.appspot.com',
+    messagingSenderId: '627775349080',
+    appId: '1:627775349080:web:b4e38d720ec5ded855c52d',
+    measurementId: 'G-ZJ0T3NYPCM',
+};
+firebase.initializeApp(firebaseConfig);
 const app = createApp(App);
 app.component('Login', Login);
 app.use(router).mount('#app');
