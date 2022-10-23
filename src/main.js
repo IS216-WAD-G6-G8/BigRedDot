0955b5f092b1b682ef7f6f10583c2a63faad99cf
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2022-10-23 19:20:18 +0800

    Create pop up for log in / sign up

--- src/main.js
+++ src/main.js
@@ -3,5 +3,17 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-// import 'fireebaseui/dist/firebaseui.css'
-createApp(App).use(router).use(store).mount('#app');
+import firebase from 'firebase/compat/app';
+import RegisterModal from './components/Reusables/RegisterModal.vue';
+const firebaseConfig = {
+    apiKey: 'AIzaSyAipvvlbfDtRMXnN_UDeHl67l-XbgWc_CU',
+    authDomain: 'is216-bigreddot.firebaseapp.com',
+    databaseURL: 'https://is216-bigreddot-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'is216-bigreddot',
+    storageBucket: 'is216-bigreddot.appspot.com',
+    messagingSenderId: '627775349080',
+    appId: '1:627775349080:web:b4e38d720ec5ded855c52d',
+    measurementId: 'G-ZJ0T3NYPCM',
+};
+firebase.initializeApp(firebaseConfig);
+createApp(App).component('Login', RegisterModal).use(router).use(store).mount('#app');
