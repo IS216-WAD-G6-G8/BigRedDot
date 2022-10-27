029ac723eb36c54d1640228ae72e5cf2d35c7c36
nicamanas <xyukimiku@gmail.com>
2022-10-27 23:27:00 +0800

    fix createUser put

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -6,7 +6,7 @@ export class UserService {
 
     constructor() {
         this.baseUrl =
-            'https://is216-bigreddot-default-rtdb.asia-southeast1.firebasedatabase.app/users.json'
+            'https://is216-bigreddot-default-rtdb.asia-southeast1.firebasedatabase.app/users'
     }
 
     async createUser(user: MultiFactorInfo) {
@@ -15,10 +15,9 @@ export class UserService {
             uid: user.uid,
             name: user.displayName,
         }
+        const createUserUrl = this.baseUrl + user.displayName + '.json'
         try {
-            let res = await axios.put(this.baseUrl, {
-                name: tempUserEntity,
-            })
+            let res = await axios.put(createUserUrl, { tempUserEntity })
             console.log(res)
         } catch (err) {
             throw err
