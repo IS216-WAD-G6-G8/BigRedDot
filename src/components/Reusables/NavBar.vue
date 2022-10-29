3c2b081c96202dd5c284bf3fac975f055597c364
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2022-10-30 01:57:43 +0800

    Fix dark mode issue

--- src/components/Reusables/NavBar.vue
+++ src/components/Reusables/NavBar.vue
@@ -211,7 +211,7 @@ export default {
                                             class="flex items-center p-5 border-b border-solid border-slate-200 rounded-t">
                                             <button
                                                 class="bg-transparent"
-                                                @click="closelogin()">
+                                                @click="showModal()">
                                                 <img
                                                     class="w-4"
                                                     src="/assets/cross.svg" />
@@ -227,7 +227,7 @@ export default {
                                                 <div>
                                                     <label
                                                         for="username"
-                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                                        class="block mb-2 text-sm font-medium text-gray-900"
                                                         >Display Name</label
                                                     >
                                                     <input
@@ -240,20 +240,20 @@ export default {
                                                 <div>
                                                     <label
                                                         for="email"
-                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                                        class="block mb-2 text-sm font-medium text-gray-900"
                                                         >Your email</label
                                                     >
                                                     <input
                                                         type="email"
                                                         name="email"
                                                         id="email"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                         placeholder="name@company.com" />
                                                 </div>
                                                 <div>
                                                     <label
                                                         for="password"
-                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                                        class="block mb-2 text-sm font-medium text-gray-900"
                                                         >Password</label
                                                     >
                                                     <input
@@ -261,12 +261,12 @@ export default {
                                                         name="password"
                                                         id="password"
                                                         placeholder="••••••••"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                                                 </div>
                                                 <div>
                                                     <label
                                                         for="confirm-password"
-                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                                        class="block mb-2 text-sm font-medium text-gray-900"
                                                         >Confirm password</label
                                                     >
                                                     <input
@@ -323,20 +323,20 @@ export default {
                                                 <div>
                                                     <label
                                                         for="email"
-                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                                        class="block mb-2 text-sm font-medium text-gray-900"
                                                         >Your email</label
                                                     >
                                                     <input
                                                         type="email"
                                                         name="email"
                                                         id="email"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                         placeholder="name@company.com" />
                                                 </div>
                                                 <div>
                                                     <label
                                                         for="password"
-                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                                        class="block mb-2 text-sm font-medium text-gray-900"
                                                         >Password</label
                                                     >
                                                     <input
