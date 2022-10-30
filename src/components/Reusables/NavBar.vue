a4f014885344f91eff81a7278909cd8441e152d8
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2022-10-30 16:17:06 +0800

    Refactor nav bar component

--- src/components/Reusables/NavBar.vue
+++ src/components/Reusables/NavBar.vue
@@ -4,6 +4,9 @@ import * as firebaseui from 'firebaseui'
 import 'firebaseui/dist/firebaseui.css'
 import { getAuth, signOut } from 'firebase/auth'
 import { UserService } from '../../services/userService'
+import SignUpModal from './SignUpModal.vue'
+import LogInModal from './LogInModal.vue'
+import ProfileDropDown from './ProfileDropDown.vue'
 
 const userService = new UserService()
 
@@ -17,6 +20,8 @@ export default {
             modal_visible: false,
             login_visible: false,
             profile: false,
+            valid_email1: true,
+            valid_email2: true,
         }
     },
     mounted() {
@@ -88,9 +93,35 @@ export default {
             this.showModal()
             this.login_visible = !this.login_visible
         },
-        closelogin(){
+        closelogin() {
             this.login_visible = false
-        }
+            this.valid_email2 = true
+        },
+        validateEmail1() {
+            let email = (<HTMLInputElement>document.getElementById('email1'))
+                .value
+            console.log(email)
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+                this.valid_email1 = true
+            } else {
+                this.valid_email1 = false
+            }
+        },
+        validateEmail2() {
+            let email = (<HTMLInputElement>document.getElementById('email2'))
+                .value
+            console.log(email)
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+                this.valid_email2 = true
+            } else {
+                this.valid_email2 = false
+            }
+        },
+    },
+    components: {
+        SignUpModal,
+        LogInModal,
+        ProfileDropDown,
     },
 }
 </script>
@@ -200,163 +231,17 @@ export default {
                                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded w-full md:w-auto">
                                 Sign Up / Log In
                             </button>
-                            <div
+                            <SignUpModal
                                 v-show="modal_visible"
-                                class="fixed inset-0 z-50 justify-center items-center flex bg-slate-500/60">
-                                <div
-                                    class="relative w-auto my-6 mx-auto max-w-6xl">
-                                    <div
-                                        class="border-0 rounded-2xl overflow-auto text-left md:rounded-lg shadow-lg relative flex flex-col md:w-full md:h-full bg-white md:min-w-[500px] max-h-[600px] w-[300px] md:min-h-[380px]">
-                                        <div
-                                            class="flex items-center p-5 border-b border-solid border-slate-200 rounded-t">
-                                            <button
-                                                class="bg-transparent"
-                                                @click="showModal()">
-                                                <img
-                                                    class="w-4"
-                                                    src="/assets/cross.svg" />
-                                            </button>
-                                            <h3
-                                                class="text-lg pl-1 font-semibold text-gray-700">
-                                                Log in or Sign up
-                                            </h3>
-                                        </div>
-                                        <div class="relative p-6 flex-auto">
-                                            <form
-                                                class="space-y-4 md:space-y-6">
-                                                <div>
-                                                    <label
-                                                        for="username"
-                                                        class="block mb-2 text-sm font-medium text-gray-900"
-                                                        >Display Name</label
-                                                    >
-                                                    <input
-                                                        type="username"
-                                                        name="username"
-                                                        id="username"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                        placeholder="Nicholas Lee" />
-                                                </div>
-                                                <div>
-                                                    <label
-                                                        for="email"
-                                                        class="block mb-2 text-sm font-medium text-gray-900"
-                                                        >Your email</label
-                                                    >
-                                                    <input
-                                                        type="email"
-                                                        name="email"
-                                                        id="email"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                        placeholder="name@company.com" />
-                                                </div>
-                                                <div>
-                                                    <label
-                                                        for="password"
-                                                        class="block mb-2 text-sm font-medium text-gray-900"
-                                                        >Password</label
-                                                    >
-                                                    <input
-                                                        type="password"
-                                                        name="password"
-                                                        id="password"
-                                                        placeholder="••••••••"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                                                </div>
-                                                <div>
-                                                    <label
-                                                        for="confirm-password"
-                                                        class="block mb-2 text-sm font-medium text-gray-900"
-                                                        >Confirm password</label
-                                                    >
-                                                    <input
-                                                        type="confirm-password"
-                                                        name="confirm-password"
-                                                        id="confirm-password"
-                                                        placeholder="••••••••"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                                                </div>
-                                                <button
-                                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded w-full">
-                                                    Create an account
-                                                </button>
-                                                <p
-                                                    class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                                    Already have an account?
-                                                    <a
-                                                        @click="openlogin()"
-                                                        class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                                                        >Login here</a
-                                                    >
-                                                </p>
-                                                <section
-                                                    id="firebaseui-auth-container"></section>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
+                                :showModal="showModal"
+                                :validateEmail1="validateEmail1"
+                                :valid_email1="valid_email1"
+                                :openlogin="openlogin" />
+                            <LogInModal
                                 v-show="login_visible"
-                                class="fixed inset-0 z-50 justify-center items-center flex bg-slate-500/60">
-                                <div
-                                    class="relative w-auto my-6 mx-auto max-w-6xl">
-                                    <div
-                                        class="border-0 rounded-2xl overflow-auto text-left md:rounded-lg shadow-lg relative flex flex-col md:w-full md:h-full bg-white md:min-w-[500px] max-h-[600px] w-[300px] md:min-h-[380px]">
-                                        <div
-                                            class="flex items-center p-5 border-b border-solid border-slate-200 rounded-t">
-                                            <button
-                                                class="bg-transparent"
-                                                @click="closelogin()">
-                                                <img
-                                                    class="w-4"
-                                                    src="/assets/cross.svg" />
-                                            </button>
-                                            <h3
-                                                class="text-lg pl-1 font-semibold text-gray-700">
-                                                Log in or Sign up
-                                            </h3>
-                                        </div>
-                                        <div class="relative p-6 flex-auto">
-                                            <form
-                                                class="space-y-4 md:space-y-6">
-                                                <div>
-                                                    <label
-                                                        for="email"
-                                                        class="block mb-2 text-sm font-medium text-gray-900"
-                                                        >Your email</label
-                                                    >
-                                                    <input
-                                                        type="email"
-                                                        name="email"
-                                                        id="email"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                        placeholder="name@company.com" />
-                                                </div>
-                                                <div>
-                                                    <label
-                                                        for="password"
-                                                        class="block mb-2 text-sm font-medium text-gray-900"
-                                                        >Password</label
-                                                    >
-                                                    <input
-                                                        type="password"
-                                                        name="password"
-                                                        id="password"
-                                                        placeholder="••••••••"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                                                </div>
-                                                <button
-                                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded w-full">
-                                                    Create an account
-                                                </button>
-                                                <section
-                                                    id="firebaseui-auth-container"></section>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                                :valid_email2="valid_email2"
+                                :validateEmail2="validateEmail2"
+                                :closelogin="closelogin" />
                         </li>
                         <li
                             v-if="$store.state.user"
@@ -387,65 +272,11 @@ export default {
                                 Sign Out
                             </button>
                         </li>
-                        <li
+                        <ProfileDropDown
                             v-if="$store.state.user"
-                            class="w-full hidden md:block md:w-auto mb-1 mt-3 md:mt-0 md:mb-0">
-                            <div class="relative border rounded-2xl">
-                                <button
-                                    id="profile_button"
-                                    @click="toggleProfile"
-                                    data-collapse-toggle="navbar-default"
-                                    type="button"
-                                    class="mx-auto flex p-2 text-sm text-gray-500 rounded-lg dark:text-gray-400"
-                                    aria-controls="navbar-default">
-                                    <svg
-                                        class="w-6 h-6"
-                                        aria-hidden="true"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            fill-rule="evenodd"
-                                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                    <img
-                                        :style="[
-                                            this.$store.getters.getDarkMode
-                                                ? {
-                                                      filter: 'brightness(0) saturate(100%) invert(98%) sepia(98%) saturate(6%) hue-rotate(127deg) brightness(102%) contrast(103%)',
-                                                  }
-                                                : { filter: 'none' },
-                                        ]"
-                                        class="ml-3"
-                                        src="/assets/profile.svg" />
-                                </button>
-                            </div>
-                            <div :class="profile ? 'block' : 'hidden'">
-                                <div
-                                    class="absolute gap-5 flex flex-col z-50 right-28 mt-2 w-52 p-5 rounded-md bg-white shadow-lg"
-                                    aria-orientation="vertical"
-                                    aria-labelledby="menu-button">
-                                    <router-link to="/MyList">
-                                        <a
-                                            class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 d dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
-                                            My Favourites
-                                        </a>
-                                    </router-link>
-                                    <router-link to="/About">
-                                        <a
-                                            class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
-                                            Profile Settings
-                                        </a>
-                                    </router-link>
-                                    <button
-                                        @click="logout()"
-                                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded w-full md:w-auto">
-                                        Sign Out
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
+                            :toggleProfile="toggleProfile"
+                            :profile="profile"
+                            :logout="logout" />
                     </ul>
                 </div>
             </div>
---
+++ src/components/Reusables/ProfileDropDown.vue
@@ -0,0 +1,72 @@
+<script lang="ts">
+
+export default {
+    name: "ProfileDropdown",
+    props: {
+        toggleProfile: Function,
+        profile: Boolean,
+        logout: Function,
+    }
+}
+
+</script>
+
+<template>
+    <li class="w-full hidden md:block md:w-auto mb-1 mt-3 md:mt-0 md:mb-0">
+        <div class="relative border rounded-2xl">
+            <button
+                id="profile_button"
+                @click="toggleProfile()"
+                data-collapse-toggle="navbar-default"
+                type="button"
+                class="mx-auto flex p-2 text-sm text-gray-500 rounded-lg dark:text-gray-400"
+                aria-controls="navbar-default">
+                <svg
+                    class="w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        fill-rule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                        clip-rule="evenodd"></path>
+                </svg>
+                <img
+                    :style="[
+                        this.$store.getters.getDarkMode
+                            ? {
+                                  filter: 'brightness(0) saturate(100%) invert(98%) sepia(98%) saturate(6%) hue-rotate(127deg) brightness(102%) contrast(103%)',
+                              }
+                            : { filter: 'none' },
+                    ]"
+                    class="ml-3"
+                    src="/assets/profile.svg" />
+            </button>
+        </div>
+        <div :class="profile ? 'block' : 'hidden'">
+            <div
+                class="absolute gap-5 flex flex-col z-50 right-28 mt-2 w-52 p-5 rounded-md bg-white shadow-lg"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button">
+                <router-link to="/MyList">
+                    <a
+                        class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 d dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                        My Favourites
+                    </a>
+                </router-link>
+                <router-link to="/About">
+                    <a
+                        class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                        Profile Settings
+                    </a>
+                </router-link>
+                <button
+                    @click="logout()"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded w-full md:w-auto">
+                    Sign Out
+                </button>
+            </div>
+        </div>
+    </li>
+</template>
