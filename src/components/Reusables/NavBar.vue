ed19771ab0a9cc819e3ab25ecfddfa723f5e73db
nicamanas <xyukimiku@gmail.com>
2022-11-07 13:08:34 +0800

    adjusted console log message

--- src/components/Reusables/NavBar.vue
+++ src/components/Reusables/NavBar.vue
@@ -102,7 +102,7 @@ export default {
             // once user is created it will auto log in
             createUserWithEmailAndPassword(auth, data.email, data.password)
                 .then((userCredential) => {
-                    console.log(userCredential.user)
+                    console.log("result:", userCredential)
                     this.showModal()
                 })
                 .catch((error) => {
