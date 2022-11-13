dae9a89d7c6e24e0cc314ee5b8db20515ecbcd54
nicamanas <xyukimiku@gmail.com>
2022-11-13 13:11:51 +0800

    set getting of data to the created stage

--- src/components/Reusables/BusinessDetail.vue
+++ src/components/Reusables/BusinessDetail.vue
@@ -49,7 +49,7 @@ export default defineComponent({
             error: false,
         }
     },
-    beforeMount() {
+    created() {
         this.getDataByID(this.business_id)
     },
     mounted() {
